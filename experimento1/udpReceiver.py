3933ad1d166a0f7bec8c31a43b9bae7e9830dcd1
João Eurípedes Pereira Júnior <joaoepj@gmail.com>
2025-03-23 12:26:47 -0300

    Testando portas UDP

--- experimento1/udpReceiver.py
+++ experimento1/udpReceiver.py
@@ -1,7 +1,7 @@
 import socket
  
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5000
+UDP_PORT = 50000
  
 sock = socket.socket(socket.AF_INET, # Internet
                       socket.SOCK_DGRAM) # UDP
--- experimento2/udpSender.py
+++ experimento2/udpSender.py
@@ -1,20 +1,19 @@
 import socket
  
-UDP_IP = "44.201.233.10"
-UDP_PORT = 5000
-MESSAGE = b'Eu sou o Datagrama numero'
+host = "44.201.233.10"
+port = 50000
+message = b'Eu sou o Datagrama numero'
 
  
-print("UDP target IP: %s" % UDP_IP)
-print("UDP target port: %s" % UDP_PORT)
-print("message: %s" % MESSAGE.decode()) # decode() converts bytearray to string
  
 sock = socket.socket(socket.AF_INET, # Internet
     socket.SOCK_DGRAM) # UDP
 
+# .decode() converts bytearray to string
 #while True:
 #MESSAGE = counter.to_bytes(1)
 #counter = counter + 1
 
 #for counter = 1
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+print("Enviando uma mensagem para o IP: " + str(host) + ", porta " + str(port))
+sock.sendto(message, (host, port))
