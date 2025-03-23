1f57f6826c323f61439de6b89119e157b6af7192
João Eurípedes Pereira Júnior <joaoepj@gmail.com>
2025-03-23 12:11:50 -0300

    Typo fix

--- experimento1/udpReceiver.py
+++ experimento1/udpReceiver.py
@@ -10,7 +10,7 @@ sock.bind((UDP_IP, UDP_PORT))
 
 print("udpReceiver aguardando a chegada de datagramas no IP "
        + socket.gethostbyname(socket.gethostname())
-       + ", porta " % UDP_PORT)
+       + ", porta " + str(UDP_PORT))
  
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
