1da94a59c5574060679992ef1c31be8abf251c35
João Eurípedes Pereira Júnior <joaoepj@gmail.com>
2025-03-31 15:38:56 -0300

    Try again

--- experimento3/udpReceiver.py
+++ experimento3/udpReceiver.py
@@ -12,6 +12,7 @@ sock.bind((UDP_IP, UDP_PORT))
 print("udpReceiver aguardando a chegada de datagramas no IP "
        + socket.gethostbyname(socket.gethostname())
        + ", porta " + str(UDP_PORT))
+
  
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
@@ -21,3 +22,4 @@ while True:
 
     if data.decode() == "fim":
        sock.sendto(bytearray("\n" + accMessage, "utf-8"), addr)
+       accMessage = ""
