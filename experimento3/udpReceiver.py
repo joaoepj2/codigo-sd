f1bd4d9d3a3c0d8524aee39a6b4f2aa26c3e0010
João Eurípedes Pereira Júnior <joaoepj@gmail.com>
2025-03-31 11:59:29 -0300

    Esqueci de salvar o arquivo

--- experimento3/udpReceiver.py
+++ experimento3/udpReceiver.py
@@ -1,7 +1,7 @@
 import socket
  
 UDP_IP = "0.0.0.0"
-UDP_PORT = 50000
+UDP_PORT = 50003
  
 sock = socket.socket(socket.AF_INET, # Internet
                       socket.SOCK_DGRAM) # UDP
@@ -15,4 +15,6 @@ print("udpReceiver aguardando a chegada de datagramas no IP "
  
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print("The message: %s was received from %s" % (data.decode(), addr))
+    consoleMessage = data.decode() + "vindo do IP e Porta: " + str(addr)
+    print(consoleMessage)
+    sock.sendto(bytearray(consoleMessage, "utf-8"), addr)
