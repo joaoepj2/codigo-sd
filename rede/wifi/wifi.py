b018957e7c031befc79ad021dc351da7cd126a35
João Eurípedes Pereira Júnior <joaoepj@gmail.com>
2025-09-24 13:40:33 -0300

    Testes com wifi

---
+++ rede/wifi/wifi.py
@@ -0,0 +1,17 @@
+﻿# lib is too old
+# it doesn't run on python 3
+
+import wifi
+
+for iface in interfaces
+    print(f"Interface Wifi: " {iface.name()})
+
+cells = wifi.Cell.all(iface)
+
+
+for cell in cells:
+    print(f"Rede: {cell.ssid}")
+    print(f"  Força do sinal (dBm): {cell.signal}")
+    print(f"  Criptografado? {'Sim' if cell.encrypted else 'Não'}")
+    print(f"  Tipo de segurança: {cell.authentication_type}")
+    print("---")
