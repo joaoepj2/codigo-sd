4a6c41488f659eb7f822fdb56881fad7594822e2
joaoepj2 <joaoepj@gmail.com>
2026-05-04 23:14:31 -0300

    Delete processador directory

--- processador/laco_infinito.py
+++
@@ -1,5 +0,0 @@
-contador = 0
-
-while True:
-    print(contador)
-    contador = contador + 1
--- processador/processo_informacoes.py
+++
@@ -1,9 +0,0 @@
-import psutil
-import os
-
-def info_processo():
-    p = psutil.Process(os.getpid())
-    print(f"PID: {p.pid}")
-    print(f"Uso de memória: {p.memory_info().rss / 1024**2:.2f} MB")
-
-info_processo()
